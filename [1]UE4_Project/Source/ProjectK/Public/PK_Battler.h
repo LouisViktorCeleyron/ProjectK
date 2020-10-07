4fb59c1b5ac186341639d975b6b4c48bff9e03c9
Louis Celeyron <louis.celeyron@gmail.com>
2020-10-07 11:57:18 +0100

    1st Commit avec les bons gitignore

---
+++ [1]UE4_Project/Source/ProjectK/Public/PK_Battler.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PK_Battler.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTK_API UPK_Battler : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
