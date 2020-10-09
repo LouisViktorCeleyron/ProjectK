99b008e9cc0cc0326cfd79ff518ec5ad6ceadb38
Louis Celeyron <louis.celeyron@gmail.com>
2020-10-09 17:22:51 +0100

    Lot of thing but it works
    Like basics for the battle and stuffs

---
+++ [1]UE4_Project/ProjectK/Source/ProjectK/Public/PK_BattleMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PK_BattleMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTK_API APK_BattleMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
